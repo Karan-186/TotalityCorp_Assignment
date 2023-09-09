4c8ca2a45d014550b3d38aaa2699ad2129c3c3e0
Karan_186 <karanking547@gmail.com>
2023-09-09 13:39:46 +0530

    Initial Commit

---
+++ TotalityCorp/TotalityCorpApp.swift
@@ -0,0 +1,17 @@
+//
+//  TotalityCorpApp.swift
+//  TotalityCorp
+//
+//  Created by KARAN D on 09/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TotalityCorpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
